2deb66392466ecdde1f20700d7b2f9bee7145b08
Ryan Tablada <ryan.tablada@gmail.com>
2019-03-15 16:39:04 -0500

    start template examples classic

---
+++ app/components/add-numbers/component.js
@@ -0,0 +1,7 @@
+import Component from '@ember/component';
+
+export default class AddNumbersComponent extends Component {
+
+}
+
+AddNumbersComponent.positionalParams = ['label'];
---
+++ app/helpers/post.js
@@ -0,0 +1,7 @@
+import { helper } from '@ember/component/helper';
+
+export function post(params/*, hash*/) {
+  return params;
+}
+
+export default helper(post);
