94e21ebd4f6ea592e6bca23441cf0dfae794f467
Ryan Tablada <ryan.tablada@gmail.com>
2019-03-18 08:27:37 -0700

    book store start

--- app/components/book-rating/component.js
+++ app/components/book-rating/component.js
@@ -4,7 +4,14 @@ import Component from '@ember/component';
 export default Component.extend({
   tagName: 'li',
   classNames: ['review'],
+  classNameBindings: ['ratingColor'],
   ratingColor: computed('rating.value', function() {
-    return this.get('rating.value') > 3 ? 'gold' : 'red';
+    if (this.get('rating.value') >= 8) {
+      return 'green';
+    }
+
+    if (this.get('rating.value') <= 3) {
+      return 'red';
+    }
   })
 });
