98694ea8bf92f78f942d600ff2b6cb13f2fbace0
Ryan Tablada <ryan.tablada@gmail.com>
2019-03-22 15:03:52 -0500

    Exercise 2 Answer

--- app/components/book-form/component.js
+++ app/components/book-form/component.js
@@ -1,39 +1,41 @@
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 
-export default Component.extend({
-  store: service('store'),
+export default class BookForm extends Component {
+  @service store
+  @tracked title;
+  @tracked isbn;
+  @tracked publishDate;
+  @tracked author;
 
-  didReceiveAttrs() {
-    this._super(...arguments);
+  constructor() {
+    super(...arguments);
 
-    this.setProperties({
-      title: this.get('book.title'),
-      isbn: this.get('book.isbn'),
-      publishDate: this.get('book.publishDate'),
-      author: this.get('book.author'),
-    });
-  },
-
-  actions: {
-    changeAuthor(id) {
-      let author = this.get('authors').find(a => a.id == id);
-
-      this.set('author', author);
-    },
-
-    searchAuthor(query) {
-
-      return this.get('store').query('author', { filter: { query }});
-    },
-
-    submitChanges() {
-      this.onsubmit({
-        title: this.title,
-        isbn: this.isbn,
-        publishDate: this.publishDate,
-        author: this.author,
-      });
-    }
+    this.title = this.args.book.title;
+    this.isbn = this.args.book.isbn;
+    this.publishDate = this.args.book.publishDate;
+    this.author = this.args.book.author;
   }
-});
+
+  @action
+  changeAuthor(id) {
+    this.author = this.args.authors.find(a => a.id == id);
+  }
+
+  @action
+  searchAuthor(query) {
+    return this.store.query('author', { filter: { query }});
+  }
+
+  @action
+  submitChanges() {
+    this.args.onsubmit({
+      title: this.title,
+      isbn: this.isbn,
+      publishDate: this.publishDate,
+      author: this.author,
+    });
+  }
+}
